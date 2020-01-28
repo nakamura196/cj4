f5fed8121ab784cd5f91ee8f2c6b750e7a60e886
Satoru Nakamura <na.kamura.1263@gmail.com>
2020-01-29 00:21:27 +0900

    bug fix

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -2,21 +2,9 @@
   <section class="container">
     <div>
       <h1>usersのindexページです。</h1>
-      <p>count={{count}}</p>
-      <button @click="addCount">カウントアップ</button>
     </div>
   </section>
 </template>
 
 <script>
-export default {
-  computed: {
-    count () { return this.$store.state.counter.count }
-  },
-  methods: {
-    addCount (e) {
-      this.$store.commit('counter/add')
-    }
-  }
-}
 </script>
